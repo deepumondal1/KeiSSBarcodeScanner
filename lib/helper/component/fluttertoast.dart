6b42459333d78ab82e71c248ab11537d8295f67e
Deepu <biplobmondal150895@gmail.com>
2021-01-28 05:44:40 +0000

    Add Dialog box for result showing

--- lib/helper/component/fluttertoast.dart
+++ lib/helper/component/fluttertoast.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class FToast{
-  static fcreatetoast({@required String msg, Color backgroundColor}){
+class FToast {
+  static fcreatetoast({@required String msg, Color backgroundColor}) {
     return Fluttertoast.showToast(
-      msg: msg,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      backgroundColor: backgroundColor ?? Colors.red,
-      textColor: Colors.white,
-      fontSize: 16.0
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: backgroundColor ?? Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  static dialogBox(BuildContext context,{Widget title, String result, Color color}) {
+    return showDialog(
+      context: context, 
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: title ?? Text("Result"),
+          content: Text(result, style: TextStyle(color: color)),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed:(){
+                Navigator.of(context).pop();
+              }
+            )
+          ]
+        );
+      }
     );
   }
 }
